54f9bc7b8966304d898fe606730c191e6985d47b
Michael Hunter <mhunter@saymedia.com>
2013-12-17 08:01:26 -0800

    Problem 3: Largest prime factor
    I also refactored the prime library.  Not entirely happy with its
    interface.  The use of a generator makes it smaller but I suspect
    slower.  I'll have to build test for both the performance and the
    results.

--- src/common/primes.py
+++ src/common/primes.py
@@ -1,17 +1,37 @@
-prime_cache = [2]
+prime_cache = [2, 3]
 
-def compute_primes(number_of_primes):
-    number = prime_cache[-1] + 1
-    while len(prime_cache) < number_of_primes:
+# TODO: If there are multiple generators then this cache isn't safe.
+def next_prime():
+    for prime in prime_cache:
+        yield prime
+    number = prime + 2
+    while True:
         for divisor in prime_cache:
+            if number % divisor == 0:
+                number += 2
+                break
             if divisor*divisor > number:
                 prime_cache.append(number)
+                yield(number)
+                number += 2
                 break
-            if number % divisor == 0:
-                break
-        else:
-            prime_cache.append(number)
-        number += 1
+
+def compute_primes(number_of_primes):
+    # prime (heh) cache
+    for n in next_prime():
+        if len(prime_cache) > number_of_primes:
+            break
     return prime_cache[:number_of_primes]
 
-
+def factor(number):
+    if prime_cache[-1]*prime_cache[-1] < number:
+        for n in next_prime():
+            if n*n > number:
+                break
+    factors = []
+    for n in next_prime():
+        if n*n > number:
+            break
+        if number % n == 0:
+            factors.append(n)
+    return factors
