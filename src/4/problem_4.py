5249d7b9755a2bb7a2261d10af240c4e3cfca442
Michael Hunter <mhunter@saymedia.com>
2013-12-23 21:56:15 -0800

    Problem 4 should only print out the answer.

--- src/4/problem_4.py
+++ src/4/problem_4.py
@@ -6,4 +6,4 @@ for mult1 in range(999, 99, -1):
             number_str = str(number)
             if number_str == number_str[::-1]:
                 largest = (mult1*mult2, mult1, mult2)
-print largest
+print largest[0]
