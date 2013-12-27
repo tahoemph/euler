ab25bae582b1ddd0f6d2006cd49aacab6b2db792
Michael Hunter <mhunter@saymedia.com>
2013-12-27 06:18:05 -0800

    Add simple memoization class.

---
+++ src/common/memoize.py
@@ -0,0 +1,11 @@
+# If we were using Python 3.2 we could use functools.lru_cache, but we are
+# not.
+class Memoize:
+    def __init__(self, f):
+        self.f = f
+        self.memo = {}
+
+    def __call__(self, *args):
+        if not args in self.memo:
+            self.memo[args] = self.f(*args)
+        return self.memo[args]
