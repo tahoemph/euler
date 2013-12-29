1a68fa2b73c0f06e6320d8d6f89484834beb88b9
Michael Hunter <mhunter@saymedia.com>
2013-12-28 18:36:50 -0800

    Remove old directory structure.

--- src/6/problem_6.py
+++
@@ -1,3 +0,0 @@
-sum_x = sum(x for x in range(1,101))
-sum_square_x = sum(x*x for x in range(1,101))
-print sum_x*sum_x - sum_square_x
--- src/7/problem_7.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append('..')
-
-import common.primes
-
-if __name__ == "__main__":
-    primes = common.primes.compute_primes(10001);
-    print primes[-1]
